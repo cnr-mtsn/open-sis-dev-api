6ca8e2eca76926c9432277d0cb17981dd2e51aa1
Conner Matson <conner_matson@science.edu>
2022-01-13 12:07:05 -0600

    update variable names to match react-app state

--- schema/student.js
+++ schema/student.js
@@ -2,24 +2,24 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const student = new Schema({
-	student_id: Number,
-	first_name: String,
-	middle_name: String,
-	last_name: String,
-	name_suffix: String,
+	studentID: Number,
+	firstName: String,
+	middleName: String,
+	lastName: String,
+	nameSuffix: String,
 	gender: String,
-	ethnicity_id: Number,
-	common_name: String,
-	social_security: String,
+	ethnicityID: Number,
+	commonName: String,
+	socialSecurity: String,
 	birthdate: String,
-	language_id: String,
-	estimated_grad_date: String,
-	alt_id: String,
+	languageID: String,
+	estimatedGradDate: String,
+	altID: String,
 	email: String,
 	phone: String,
-	is_disable: String,
-	last_updated: Date,
-	updated_by: String,
+	isDisable: String,
+	lastUpdated: Date,
+	updatedBy: String,
 });
 
 const Student = mongoose.model("student", student);
